dd9f705ae50bc947d18a559ca5f2e144f23b3b91
Steffen Huester <steffen.huester@sap.com>
2023-05-31 16:39:31 +0200

    FLP Sandbox config sample w/ SAP Companion
    Sample configuration for the SAP Companion
    (aka Web Assistant) plugin and its usage with
    the cds-launchpad-plugin
    
    CAUTION: the FLP runtime configuration is not
    a public API contract. Breaking changes might
    be applied also in minor SAPUI5 version
    upgrades.

--- fiori/server.js
+++ fiori/server.js
@@ -5,4 +5,13 @@ const opts = global.it ? { target:'auto' } : {}   // for tests, set 'auto' to de
 cds.on('bootstrap', app => app.use(proxy(opts)))  // install proxy
 cds.log('cov2ap','silent') // suppress anoying log outpout, e.g. for `npm run mocha -- --reporter nyan`
 
+if (process.env.NODE_ENV !== 'production') {
+  // Enable launchpad plugin
+  const { cds_launchpad_plugin } = require('cds-launchpad-plugin');
+  cds.once('bootstrap', (app) => {
+    const handler = new cds_launchpad_plugin();
+    app.use(handler.setup({ theme: 'sap_horizon', appConfigPath: cds.root + '/app/appconfig/fioriSandboxPlugins.json' }));
+  });
+}
+
 module.exports = require('@capire/bookstore/server.js')
